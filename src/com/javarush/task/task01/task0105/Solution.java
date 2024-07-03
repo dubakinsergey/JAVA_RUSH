1eeb539488fb96814c2329dc90cb890dcfaa3176
Sergey Dubakin <dubakinsergey@gmail.com>
2024-07-03 21:15:45 +0500

    level 1 lecture 3

---
+++ src/com/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task01.task0105;
+
+/* 
+Объявляем переменные
+
+Напиши программу, которая в методе main объявляет такие переменные:
+name типа String, age типа int и city типа String.
+
+Примечание: "объявить переменную" - значит то же, что и "создать переменную".
+
+Требования:
+•	Объяви переменную name типа String.
+•	Объяви переменную age типа int.
+•	Объяви переменную city типа String.
+•	Должны быть объявлены 3 переменны
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String name;
+        int age;
+        String city;
+    }
+}
