1753c1c3c3253de94d732c353436458f0135af48
Sergey Dubakin <dubakinsergey@gmail.com>
2024-07-06 15:47:53 +0500

    level 2 lecture 3

---
+++ src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+
+В методе main объяви переменные intValue, numericValue, size, number типа int.
+Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+Значениями могут быть любые целые числа.
+
+Требования:
+•	В методе main объяви четыре переменные типа int.
+•	Имена переменных должны быть intValue, numericValue, size и number.
+•	Переменным сразу должны быть присвоены значения.
+•	Все переменные должны иметь разные значения.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int intValue = 10, numericValue = 20, size = 30, number = 40;
+    }
+}
