ffcaf9741a086e32c1bbcca3b020f7e86ee48348
Sergey Dubakin <dubakinsergey@gmail.com>
2024-07-07 15:01:36 +0500

    level 2 lecture 6

---
+++ src/com/javarush/task/pro/task02/task0211/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task02.task0211;
+
+/* 
+Получение длины строки
+
+В методе main на экран выводятся значения трех строк.
+Внеси изменения в код, чтобы вместо значений строк вывелась длина каждой строки.
+
+Требования:
+•	Не изменяй значение переменной emptyString.
+•	У трех строк, переданных в метод println, нужно вызвать метод length.
+•	Программа должна вывести на экран три числа, каждое - с новой строки.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String emptyString = "";
+
+        //напишите тут ваш код
+        System.out.println(emptyString.length());
+        System.out.println("Gomu Gomu no Bazooka!".length());
+        System.out.println((emptyString + 2 + 2 + "22").length());
+    }
+}
