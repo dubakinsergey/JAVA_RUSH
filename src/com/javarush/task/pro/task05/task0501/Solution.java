b7a95b2e4487461a8babfd3fb75f633e206d9ce9
Sergey Dubakin <dubakinsergey@gmail.com>
2024-09-19 20:33:57 +0500

    level 6 lecture 1

---
+++ src/com/javarush/task/pro/task05/task0501/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task05.task0501;
+
+/* 
+Создаем массивы
+
+В методе main проинициализируй переменные intArray и doubleArray массивами соответствующих типов, размер которых равен 10.
+
+Требования:
+•	В методе main(String[]) проинициализируй переменную intArray согласно условию.
+•	В методе main(String[]) проинициализируй переменную doubleArray согласно условию.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
